8b13f7b058d9d8c0a623d92f2f36368caceb71e9
NewNomad <tosiki1997@gmail.com>
2023-03-07 01:45:24 +0900

    rename:CheckButton.tsx

--- src/components/atoms/CheckButton.tsx
+++ src/components/atoms/CheckButton.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 type Props = {
   prefectureName: string
   checked: boolean
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
-
 export const CheckButton = (props: Props) => {
-  const { prefectureName, checked } = props
+  const { prefectureName, checked, onChange } = props
 
   return (
     <label>
-      <input type="checkbox" checked={checked} />
+      <input type="checkbox" onChange={onChange} checked={checked} />
       {prefectureName}
     </label>
   )
