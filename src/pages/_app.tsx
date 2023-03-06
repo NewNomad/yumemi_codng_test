a8ce9b8286e86b9c0b31fc569dc519b294cc1a99
NewNomad <tosiki1997@gmail.com>
2023-03-06 22:49:08 +0900

    add:constants folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
