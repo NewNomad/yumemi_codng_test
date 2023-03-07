11cc606afa2152bc08856479f8bf392ff2c0af23
NewNomad <tosiki1997@gmail.com>
2023-03-07 23:41:09 +0900

    add:fetched prefectures

--- src/components/organisms/PrefectureSelection.tsx
+++ src/components/organisms/PrefectureSelection.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import { CheckBox } from '../molecules/CheckBox';
+import { Prefecture } from 'src/types/prefectures';
 
-export const PrefectureSelection = () => {
-  return <CheckBox name="a" checked onChange={() => {}} />;
+type Props = {
+  prefectures: Prefecture[];
+};
+
+export const PrefectureSelection = (props: Props) => {
+  const { prefectures } = props;
+
+  return (
+    <>
+      {prefectures.map((prefecture) => (
+        <CheckBox
+          key={prefecture.prefCode}
+          name={prefecture.prefName}
+          checked
+          onChange={() => {}}
+        />
+      ))}
+    </>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 import { NextPage } from 'next';
 import { Header } from 'src/components/organisms/Header';
 import { PrefectureSelection } from 'src/components/organisms/PrefectureSelection';
+import { Prefectures } from 'src/types/prefectures';
 import useSWR from 'swr';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  const { data, error, isLoading } = useSWR('/api/prefectures', fetcher);
+  const { data, error, isLoading } = useSWR<Prefectures, Error>(
+    '/api/prefectures',
+    fetcher,
+  );
+  if (error) return <div>RESASの情報取得に失敗しました</div>;
+  if (isLoading) return <div>ロード中</div>;
 
-  console.log(data);
+  const prefectures = data!.result;
 
   return (
     <>
       <Header title="Title" />
-      <PrefectureSelection />
+      <PrefectureSelection prefectures={prefectures} />
     </>
   );
 };
