4df5b7c42ac6bcec47cb95424cb84edd428207af
NewNomad <tosiki1997@gmail.com>
2023-03-08 00:48:50 +0900

    add:function handlePrefectureSelection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import { Header } from 'src/components/organisms/Header';
 import { PrefectureSelection } from 'src/components/organisms/PrefectureSelection';
 import { Prefectures } from 'src/types/prefectures';
@@ -7,22 +8,32 @@ import useSWR from 'swr';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  // const { data, error, isLoading } = useSWR<Prefectures, Error>(
-  //   '/api/prefectures',
-  //   fetcher,
-  // );
-  // if (error) return <div>RESASの情報取得に失敗しました</div>;
-  // if (isLoading) return <div>ロード中</div>;
+  // 結果がほとんど変わらないので、swrでキャッシュと、エラーやロードのハンドリング
+  const { data, error, isLoading } = useSWR<Prefectures, Error>(
+    '/api/prefectures',
+    fetcher,
+  );
+  if (error) return <div>RESASの情報取得に失敗しました</div>;
+  if (isLoading) return <div>ロード中</div>;
 
-  const { data } = useSWR('api/composition?prefCode=3', fetcher);
-  console.log(data);
+  const [composition, setComposition] = useState();
 
-  // const prefectures = data!.result;
+  const handlePrefectureSelection = async () => {
+    try {
+      const response = await fetch('api/composition?prefCode=3');
+      const data = await response.json();
+      setComposition(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const prefectures = data!.result;
 
   return (
     <>
       <Header title="Title" />
-      {/* <PrefectureSelection prefectures={prefectures} /> */}
+      <PrefectureSelection prefectures={prefectures} />
     </>
   );
 };
