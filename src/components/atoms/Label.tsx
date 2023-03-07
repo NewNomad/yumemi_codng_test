4414be3a62fbd78c7aa816c67fca2909faf7fb93
NewNomad <tosiki1997@gmail.com>
2023-03-07 17:23:35 +0900

    fix:Label.tsx

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { Children, ReactNode } from 'react'
+type Props = {
+  name: string
+  children: ReactNode
+}
 
-export const Label = () => {
-  return <div>Label</div>
+export const Label = (props: Props) => {
+  const { name, children } = props
+
+  return (
+    <label>
+      {children}
+      {name}
+    </label>
+  )
 }
