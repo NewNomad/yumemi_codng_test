f68368faeed14ba993ea196a7af63ab0c25e3e0c
NewNomad <tosiki1997@gmail.com>
2023-03-07 16:29:06 +0900

    fix:index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next'
+import { Header } from 'src/components/organisms/Header'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Header title="Title" />
       <div />
     </>
   )
