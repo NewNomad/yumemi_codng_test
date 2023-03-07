676beda2666096aa7593f058908e971f1a1455bb
NewNomad <tosiki1997@gmail.com>
2023-03-07 17:48:42 +0900

    fix:index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from 'next'
 import { Header } from 'src/components/organisms/Header'
+import { PrefectureSelection } from 'src/components/organisms/PrefectureSelection'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header title="Title" />
-      <div />
+      <PrefectureSelection />
     </>
   )
 }
