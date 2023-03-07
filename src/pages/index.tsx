980693d272de7f0768d4186e9d3ecab91bc0a74b
NewNomad <tosiki1997@gmail.com>
2023-03-07 23:25:47 +0900

    add:swr to access resasAPI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { NextPage } from 'next';
 import { Header } from 'src/components/organisms/Header';
 import { PrefectureSelection } from 'src/components/organisms/PrefectureSelection';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
+  const { data, error, isLoading } = useSWR('/api/prefectures', fetcher);
+
+  console.log(data);
+
   return (
     <>
       <Header title="Title" />
