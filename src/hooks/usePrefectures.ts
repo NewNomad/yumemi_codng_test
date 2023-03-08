dcadf01f9d48237fdbe610ddb1986b4f0af2637d
NewNomad <tosiki1997@gmail.com>
2023-03-08 23:16:48 +0900

    update:hooks

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Prefecture, Prefectures } from 'src/types/prefectures';
 
-export const usePrefectures = () => {};
+export const usePrefectures = async () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [prefectures, setPrefectures] = useState<Prefecture[]>();
+
+  const getPrefectures = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const result: Prefectures = await (
+        await fetch('/api/prefectures')
+      ).json();
+      const prefectures = result.result;
+      setPrefectures(prefectures);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return { isLoading, prefectures, setPrefectures };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,6 @@ import { Prefectures } from 'src/types/prefectures';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  // const { prefectures, setPrefectures, error, isLoading } = useFetchPref();
-  // console.log(prefectures);
-
-  // if (error) return <div>RESASの情報取得に失敗しました</div>;
-  // if (isLoading) return <div>ロード中</div>;
-
-  // console.log(prefectures);
-
   const handlePrefectureSelection = async () => {
     try {
       const response = await fetch('api/composition?prefCode=3');
