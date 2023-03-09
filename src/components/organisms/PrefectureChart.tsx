7793f229d4ee9ce6952bbde929a161e24fb8943e
NewNomad <tosiki1997@gmail.com>
2023-03-09 09:43:27 +0900

    add:Line

--- src/components/organisms/PrefectureChart.tsx
+++ src/components/organisms/PrefectureChart.tsx
@@ -3,6 +3,7 @@ import {
   CartesianGrid,
   Label,
   Legend,
+  Line,
   LineChart,
   Tooltip,
   XAxis,
@@ -14,23 +15,26 @@ type Props = {
   chartComposition: chartComposition[];
 };
 
-export const PrefectureChart: React.FC<Props> = (props: Props) => {
+export const PrefectureChart = (props: Props) => {
   const { chartComposition } = props;
+
+  console.log(chartComposition);
+
+  if (!chartComposition[0]) return <></>;
 
   return (
     <section>
-      <LineChart
-        width={700}
-        height={300}
-        // data={}
-      >
+      <LineChart width={700} height={300} data={chartComposition[0].data.data}>
         <CartesianGrid strokeDasharray={3} />
-        <XAxis>
+        <XAxis dataKey="year">
           <Label value="年度" position="bottom" />
         </XAxis>
         <YAxis>
           <Label value="人口数" position="insideTopLeft" />
         </YAxis>
+        {chartComposition.map((comp, i) => (
+          <Line name={comp.prefectureName} data={i} dataKey="value" key={i} />
+        ))}
         <Legend verticalAlign="top" />
         <Tooltip />
       </LineChart>
